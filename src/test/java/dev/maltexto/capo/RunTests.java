5ec2fd18035457ce83c7396bb24f2bac1e178d53
Joás <cobraljrjoas@tutanota.com>
2023-08-24 23:29:07 -0300

    tests: :construction:

---
+++ src/test/java/dev/maltexto/capo/RunTests.java
@@ -0,0 +1,13 @@
+package dev.maltexto.capo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
