3d31c1e88bbb6984ae3bbaa8c72fdbd56d5f524a
Joás <cobraljrjoas@tutanota.com>
2023-08-24 23:16:11 -0300

    run forrest run

---
+++ src/main/java/dev/maltexto/capo/Run.java
@@ -0,0 +1,13 @@
+package dev.maltexto.capo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Run {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Run.class, args);
+	}
+
+}
