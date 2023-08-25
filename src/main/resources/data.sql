6c2544d4d0389e3786bba28b75b1d0b0f2ab7ec2
Joás <cabraljrjoas@tutanota.com>
2023-08-25 10:28:07 -0300

    chore: `data.sql`  in english  :fu:

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
 INSERT INTO product (barcode, name, minimum_quantity, initial_balance) VALUES
-('1234567890', 'Caneta', 10, 100),
-('9876543210', 'Lápis', 20, 200);
+('1234567890', 'Pen', 10, 100),
+('9876543210', 'Pencil', 20, 200);
 
 INSERT INTO movement (product_id, movement_type, quantity, date, reason, document) VALUES
-(1, 'ENTRADA', 10, '2023-08-18', 'Compra', 'NF-123456789'),
-(1, 'SAÍDA', 5, '2023-08-19', 'Venda', 'NF-987654321'),
-(1, 'INITIAL_BALANCE', 100, '2023-08-18', 'Estoque inicial', null),
-(1, 'ENTRY_ADJUSTMENT', 5, '2023-08-19', 'Ajuste de entrada', null),
-(1, 'EXIT_ADJUSTMENT', 2, '2023-08-20', 'Ajuste de saída', null);
+(1, 'ENTRY', 10, '2023-08-18', 'BUY', '123456789'),
+(1, 'EXIT', 5, '2023-08-19', 'SELL', '987654321'),
+(1, 'INITIAL_BALANCE', 100, '2023-08-18', null, null),
+(1, 'ENTRY_ADJUSTMENT', 5, '2023-08-19', null, null),
+(1, 'EXIT_ADJUSTMENT', 2, '2023-08-20', null, null);
